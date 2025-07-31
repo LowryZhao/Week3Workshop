16eb7d6e60e68d9fd845ccee5db587d25f09e28b
LowryZhao <lowryzhao.griffithuni.edu.au>
2025-08-01 01:46:40 +1000

    Initial commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+app.use(express.static(__dirname + '/www'));
+app.use(express.json());
+let http = require('http').Server(app);
+
+const users = [
+    {email: '123@qq.com', password:'123'},
+    {email: '456@qq.com', password:'456'},
+    {email: '789@qq.com', password:'7789'}
+];
+// server start
+let server = http.listen(3000,function(){
+
+    console.log('server has started')
+});
+
+app.get('/',function(req,res){
+
+    let filepath = __dirname + '/www/index.html';
+    res.sendFile(filepath)
+    console.log("Successfully: ", filepath)
+});
+
+app.get('/account', function(req,res){
+    let filepath = __dirname + '/www/account.html';
+    res.sendFile(filepath)
+    console.log("Successfully: ", filepath)
+});
+
+app.post('/api/loginform',function(req,res){
+    let response = {};
+
+        if (!req.body) {
+            console.log('error');
+          return res.sendStatus(400);
+        }
+
+        console.log(req.body);
+            response.data = req.body.data;
+            response.valid = true;
+            res.send(response);
+});
---
+++ www/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+  $('#loginForm').submit(function (event) {
+    event.preventDefault();
+
+    const email = $('#email').val();
+    const password = $('#password').val();
+
+    ajaxPost(email, password);
+  });
+
+  function ajaxPost(email, password) {
+    const formData = {
+      email: email,
+      password: password
+    };
+
+    $.ajax({
+      type: 'POST',
+      contentType: 'application/json',
+      url: window.location + "api/loginform",
+      data: JSON.stringify(formData),
+      dataType: 'json',
+      success: function (response) {
+        if (response.valid === true) {
+          console.log("good");
+          $("#result").html("We got a response: " + response.data);
+        } else {
+          $("#result").html("We got an invalid response.");
+        }
+      },
+      error: function (e) {
+        alert("Error!")
+        console.log("ERROR: ",e);
+      }
+    });
+  }
+});
